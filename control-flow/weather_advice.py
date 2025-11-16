0eeef7724354949debf885657ff3136767101499
kratxs <nwamadipaul1@gmail.com>
2025-11-16 12:21:48 +0100

    Fixed error in weather app

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,16 @@
+# Prompt User for Weather Input
 current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
+# Provide Clothing Recommendations
 if current_weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-
-elif current_weather == "cold":
-    print("Make sure to wear a warm coat and a scarf.")
 
 elif current_weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
 
+elif current_weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+
+# Unexpected Input
 else:
     print("Sorry, I don't have recommendations for this weather.")
